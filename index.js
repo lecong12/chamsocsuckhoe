ffddc315e9e7c3ea865f8161e35ccbf15e47e28a
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-08 12:55:24 -0700

    Update header text to English for consistency.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h2>Chăm Sóc Sức Khỏe Blockchain</h2>;
+  return <h2>Blockchain Health Care</h2>;
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
