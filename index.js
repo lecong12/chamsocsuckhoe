d161f233cb0058e0d7f2b3c50f26465526fb0111
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-08 12:56:15 -0700

    Update heading to H1 for better semantics

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h2>Blockchain Health Care</h2>;
+  return <h1>Blockchain Health Care</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
