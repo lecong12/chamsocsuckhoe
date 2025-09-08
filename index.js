0275ce4f03fc497181a37f1f0e19d51655431d2a
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-08 12:56:43 -0700

    Update title to reflect app purpose more clearly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>Blockchain Healthcare System</h1>;
+  return <h1>Blockchain-Based Healthcare System</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
