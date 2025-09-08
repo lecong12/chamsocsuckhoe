378c02e65a44831f6e17488e76a66229f7aa214a
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-08 12:54:24 -0700

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h2>Chăm Sóc Sức Khỏe Blockchain</h2>;
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
